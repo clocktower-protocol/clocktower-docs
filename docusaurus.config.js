80587a17881ceb1312577920660fa72c0f441205
Hugo Marx <hugo@clocktower.finance>
2025-04-17 16:18:51 -0500

    tested password protection

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,8 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://clocktowerdev.com',
+  //url: 'https://clocktowerdev.com',
+  url: 'http://localhost:8787',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
