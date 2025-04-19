4990e88158d75793355f66b6cea389e427992d21
Hugo Marx <hugo@clocktower.finance>
2025-04-19 14:29:31 -0500

    changed url to clocktower.finance

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
 
   // Set the production url of your site here
   //url: 'https://clocktowerdev.com',
-  url: 'https://clocktower-docs.pages.dev',
+  url: 'https://clocktower.finance',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
