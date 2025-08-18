c9c3f5c24e515bf8e3a975d0c5c90687e0799307
vhmarx <vhmarx@haderachllc.com>
2025-08-18 15:17:52 -0500

    created spanish home page and link to wp

--- docs/whitepaper/index.md
+++ docs/whitepaper/index.md
@@ -2,3 +2,13 @@
 id: whitepaper
 title: Whitepaper
 ---
+
+<script>
+  window.location.href = '/wp/v5_whitepaper.pdf';
+</script>
+
+# Whitepaper
+
+Redirecting to PDF...
+
+If you are not redirected automatically, [click here to download the whitepaper](/wp/v5_whitepaper.pdf).
---
+++ i18n/es/src/pages/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+//import clsx from 'clsx';
+//import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import MainImage from '@site/static/img/interior_rotated2.jpg';
+import Translate, {translate} from '@docusaurus/Translate';
+//import TowerImage from '@site/static/img/towerside.jpeg';
+import WiresImage from '@site/static/img/wires3.jpeg'
+import ChurchImage from '@site/static/img/church2.jpg'
+
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  //const {siteConfig} = useDocusaurusContext();
+  return (
+    <div>
+      <div style={{paddingTop: "20px"}}>
+        <h1 className={styles.heroBanner}>Suscripciones para Todos</h1>
+      </div>
+      <div className={styles.imageContainer}>
+        <img src={MainImage} className={styles.imageFitter} alt={
+              translate({
+                message: 'Main Image',
+                description: 'The homepage main image alt message',
+              })
+        }></img>
+      </div>
+      <div className={styles.doubleBoxGridContainer}>
+        <div className={styles.doubleBoxText}>
+          <h1 className={styles.banner2}>Pagos Cripto <p></p>Recurrentes <p></p> Descentralizados</h1>
+        </div>
+        <div className={styles.doubleBoxImage}>
+          <img className={styles.imageFitter} src={ChurchImage} alt="towerimage"></img>
+        </div>
+      </div>
+      <div className={styles.doubleBoxGridContainer}>
+        <div className={styles.doubleBoxImage}>
+            <img className={styles.imageFitter} src={WiresImage} alt="wiresimage"></img>
+        </div>
+        <div className={styles.doubleBoxText}>
+            <h1 className={styles.banner3}>Es Hora</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description={
+        translate({
+          message: 'Decentralized Subscriptions',
+          description: 'Layout Description',
+        })
+  }>
+      <HomepageHeader />
+      <main>
+       
+      </main>
+    </Layout>
+  );
+}
